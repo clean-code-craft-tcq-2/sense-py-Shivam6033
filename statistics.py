8095e9f0c63666be0e9b240859d0038cef7f29fe
Shivam <37435555+Shivam6033@users.noreply.github.com>
2021-10-25 17:41:14 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,3 +1,30 @@
 
+import math
+
 def calculateStats(numbers):
-  return None
+  if(len(numbers)>0):
+    avgValue = sum(numbers) / len(numbers)
+    minimum_value = min(numbers)
+    maximum_value = max(numbers)
+    return { 'avg':avgValue, 'min':maximum_value, 'max':minimum_value }
+  else:
+    return { 'avg':math.nan, 'min':math.nan, 'max':math.nan }
+
+class LEDAlert:
+	def __init__(self):
+		self.led_glows = False
+  
+class EmailAlert:
+  def __init__(self):
+    self.email_sent=False
+    
+class StatsAlerter:
+	def __init__(self, maxThreshold, alertList):
+		self.max_threshold = maxThreshold
+		self.alert_list = alertList
+		
+	def checkAndAlert(self, values):
+		result = calculateStats(values)
+		if result["max"] > self.maxThreshold:
+			self.alertList[0].email_sent = True
+			self.alertList[1].led_glows = True
